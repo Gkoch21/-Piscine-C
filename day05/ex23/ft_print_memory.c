eade09daa5b67a63d97fad88d9ed741af1a13240
Gkoch Timur <uryuk.uz@gmail.com>
2018-10-30 17:51:54 +0300

    add evalExpr

--- day05/ex23/ft_print_memory.c
+++ day05/ex23/ft_print_memory.c
@@ -1,21 +1,253 @@
-// #include	<stdio.h>
+// #include <stdio.h>
 
-// void	*ft_print_memory(void *addr, unsigned int size)
+// int
+// is_safe(int rows[8], int x, int y)  
 // {
-// 	int		i;
+//     int i;
+    
+//     if (y == 0)
+//             return 1;
+//     for (i=0; i < y; ++i) {
+//        if (rows[i] == x || rows[i] == x + y - i || rows[i] == x - y +i)
+//             return 0;
+//     }
+
+//     return 1;
+// }
+ 
+// void
+// putboard(int rows[8])  
+// {
+//     static int s = 0;
+//     int x, y;
+
+//     printf("\nSolution #%d:\n---------------------------------\n", ++s);
+//     for (y=0; y < 8; ++y) {
+//         for (x=0; x < 8; ++x)
+//             printf(x == rows[y] ? "| Q " : "|   ");
+//         printf("|\n---------------------------------\n");
+//     }
+// }
+ 
+// void
+// eight_queens_helper(int rows[8], int y)
+// {
+//     int x;
+
+//     for (x=0; x < 8; ++x) {
+//         if (is_safe(rows, x, y)) {
+//             rows[y] = x;
+//             if (y == 7)
+//               putboard(rows);
+//             else
+//               eight_queens_helper(rows, y+1);
+//         }
+//     }
+// }
+
+// int
+// main()
+// {
+//     int rows[8];
+
+//     eight_queens_helper(rows, 0);
+
+//     return 0;
+// }
+
+
+
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	"./libft.h"
+
+int	** board;
+
+void	ft_createboard(int n)
+{
+	int		i;
+	int		j;
+
+	free(board);
+	board	=	(int **)malloc(sizeof(int*) * n);
+	i		=	0;
+	while (i < (n))
+	{
+		board[i]	=	(int *)malloc(sizeof(int) * n);
+		j			=	0;
+		while (j < n)
+		{
+			board[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ft_printboard(int n)
+{
+	int		i;
+	int		j;
+
+	i	=	0;
+	ft_putstr("-----------------\n");
+	while (i < n)
+	{
+		j	=	0;
+		ft_putchar('|');
+		while (j < n)
+		{
+			if (board[i][j] == -1)
+				ft_putchar('Q');
+			else
+				ft_putchar('.');
+			// ft_putnbr(board[i][j]);
+			ft_putchar('|');
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	ft_putstr("-----------------\n");
+}
+
+void	setQuin(int i, int j, int n)
+{
+	int	x;
+	int	foo;
+
+	x	=	0;
+	while (x < n)
+	{
+		++board[x][j];
+		++board[i][x];
+		foo = j - i + x;
+		if (foo >= 0 && foo < n)
+			++board[x][foo];
+		foo = j + i - x;
+		if (foo >= 0 && foo < n)
+			++board[x][foo];
+		x++;
+	}
+	board[i][j] = -1;
+}
+
+void	resetQuin(int i, int j, int n)
+{
+	int	x;
+	int	foo;
+
+	x	=	0;
+	while (x < n)
+	{
+		--board[x][j];
+		--board[i][x];
+		foo = j - i + x;
+		if (foo >= 0 && foo < n)
+			--board[x][foo];
+		foo = j + i - x;
+		if (foo >= 0 && foo < n)
+			--board[x][foo];
+		x++;
+	}
+	board[i][j] = 0;
+}
+
+int		checkRow(int i, int n)
+{
+	int		j;
+
+	j	=	0;
+	while (j < n)
+	{
+		if (board[i][j] == 0)
+			return (1);
+		j++;
+	}
+	return (0);
+}
+
+int		tryQuin(int i, int n)
+{
+	int		answer;
+	int		j;
+
+	answer	=	0;
+	j	=	0;
+	if (checkRow(i, n))
+		while (j < n)
+		{
+			if (board[i][j] == 0)
+			{
+				setQuin(i, j, n);
+				if (i == (n - 1))
+					return (1);
+				else
+					if (!(answer = tryQuin((i + 1), n)))
+						resetQuin(i, j, n);
+			}
+			j++;
+		}
+	else
+		if (i < (n - 1))
+			tryQuin(i + 1, n);
+	return (answer);
+}
+
+// int		tryQuin(int i, int n)
+// {
+// 	int		answer;
 // 	int		j;
 
-// 	i = 0;
-// 	while (i < (size / 16 + ((size % 16)? 1 : 0)))
+// 	answer	=	0;
+// 	j	=	0;
+// 	while (j < n)
 // 	{
-// 		j = -1;
-// 		while ((char)addr[++j])
-// 			if(addr[j])
+// 		if (board[i][j] == 0)
+// 		{
+// 			setQuin(i, j, n);
+// 			if (i == (n - 1))
+// 				answer	=	1;
+// 			else
+// 				if (!(answer = tryQuin((i + 1), n)))
+// 					resetQuin(i, j, n);
+// 		}
+// 		if (answer)
+// 			break ;
+// 		j++;
 // 	}
+// 	return (answer);
 // }
 
-// int		main(void)
-// {
-// 	ft_print_memory("asdfasdfqwertytyzxcvzxcv\0\0\xff\x7f\x01", 29);
-// 	return (0);
-// }
+void	ft_alldecision(int n)
+{
+	int	i;
+	int	j;
+
+	printf("test\n");
+	i	=	0;
+	while (i < n)
+	{
+		j	=	0;
+		while (j < n)
+		{
+			ft_createboard(n);
+			setQuin(i, j, n);
+			if (tryQuin(0, n))
+				ft_printboard(n);
+			j++;
+		}
+		i++;
+	}
+}
+
+int		main(void)
+{
+	int		n	=	8;
+
+	ft_alldecision(n);
+	// ft_createboard(n);
+	// tryQuin(0, n);
+	// ft_printboard(n);
+	return (0);
+}
---
+++ evalExpr/srcs/main.c
@@ -0,0 +1,204 @@
+#include	"../includes/libft.h"
+
+int	ft_do_op(int i1, int i2, char str)
+{
+	if (str == '-')
+		return(i1 - i2);
+	else if (str == '*')
+		return(i1 * i2);
+	else if (str == '+')
+		return(i1 + i2);
+	else if (str == '/')
+		if (i2 == 0)
+			write(1, "Stop : division by zero", 23);
+		else
+			return(i1 / i2);
+	else if (str == '%')
+		if (i2 == 0)
+			write(1, "Stop : modulo by zero", 22);
+		else
+			return(i1 % i2);
+	else
+		return (0);
+	return (0);
+}
+
+int		btree_apply_prefix(t_eval *root)
+{
+	int		answer;
+
+	answer = 0;
+	if (root->oprt)
+	{
+		answer = ft_do_op(
+			btree_apply_prefix(root->left),
+			btree_apply_prefix(root->right),
+			root->oprt);
+		return (answer);
+	}
+	else
+		return (root->intg);
+}
+
+t_eval	*create_oprt(char	t)
+{
+	t_eval	*new;
+
+	new	=	(t_eval *)malloc(sizeof(t_eval));
+	if (!new)
+		return (NULL);
+	new->oprt	=	t;
+	new->intg	=	0;
+	new->left	=	NULL;
+	new->right	=	NULL;
+	return (new);
+}
+
+t_eval	*	create_intg(int n)
+{
+	t_eval	*new;
+
+	new	=	(t_eval *)malloc(sizeof(t_eval));
+	if (!new)
+		return (NULL);
+	new->oprt	=	'\0';
+	new->intg	=	n;
+	new->left	=	NULL;
+	new->right	=	NULL;
+	return (new);
+}
+
+int		bracket(char *str, int n)
+{
+	int		flag;
+
+	flag	=	0;
+	while (str[++n])
+	{
+		if (str[n] == '(')
+			flag++;
+		else if (str[n] == ')' && (flag == 0))
+			return (n);
+		else if (str[n] == ')' && (flag > 0))
+			flag--;
+	}
+	return (n);
+}
+
+char	*	cut_bracket(char *str)
+{
+	char	*	nstr;
+	int			n;
+
+	if (bracket(str, 0) == (ft_strlen(str) - 1))
+	{
+		nstr	=	ft_strnew((size_t)(ft_strlen(str) - 1));
+		n	=	0;
+		while (++n < ft_strlen(str) - 1)
+			nstr[n - 1] = str[n];
+		str	=	nstr;
+		return (nstr);
+	}
+	return (str);
+}
+
+char	*	cut_str(char * str, int n)
+{
+	int			i;
+	char	*	answer;
+
+	answer	=	(char *)malloc(sizeof(char) * n);
+	if (!answer)
+		return (NULL);
+	i	=	0;
+	while (i < n)
+	{
+		answer[i]	=	str[i];
+		i++;
+	}
+	answer[i]	=	'\0';
+	return (answer);
+}
+
+int		simple(char *str)
+{
+	int		n;
+	int		flag;
+
+	n		=	-1;
+	flag	=	0;
+	while (str[++n])
+	{
+		if (str[n] == '(')
+			n = bracket(str, n);
+		if (str[n] == '+' || (str[n] == '-'))
+			flag = n;
+	}
+	return (flag);
+}
+
+int		complex(char *str)
+{
+	int		n;
+	int		flag;
+
+	n		=	-1;
+	flag	=	0;
+	while (str[++n])
+	{
+		if (str[n] == '(')
+			n = bracket(str, n);
+		if (str[n] == '*' || (str[n] == '/')
+			|| (str[n] == '%'))
+			flag = n;
+	}
+	return (flag);
+}
+
+t_eval	*	create_tree(char * str)
+{
+	t_eval	*	answer;
+	int			n;
+
+	answer	=	NULL;
+	n	=	0;
+	if (str[0] == '(')
+		str	=	cut_bracket(str);
+	if ((n = simple(str)))
+	{
+		answer			=	create_oprt(str[n]);
+		answer->left	=	create_tree(cut_str(str, n));
+		answer->right	=	create_tree(&str[n + 1]);
+		return (answer);
+	}
+	else if ((n = complex(str)))
+	{
+		answer			=	create_oprt(str[n]);
+		answer->left	=	create_tree(cut_str(str, n));
+		answer->right	=	create_tree(&str[n + 1]);
+		return (answer);
+	}
+	return (create_intg(ft_atoi(str)));
+}
+
+int		eval_expr(char * str)
+{
+	t_eval	*tree;
+	int		answer;
+
+	tree	=	NULL;
+	str		=	ft_clear_char(str, ' ');
+	tree	=	create_tree(str);
+	answer	=	btree_apply_prefix(tree);
+	return(answer);
+}
+
+int main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		ft_putnbr(eval_expr(av[1]));
+		ft_putchar('\n');
+	}
+	return (0);
+}
